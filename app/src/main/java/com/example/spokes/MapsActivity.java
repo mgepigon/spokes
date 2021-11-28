bbb9293f251da6016d17e5c0b6d8c94488087e85
marc <mgepigon@ucsb.edu>
2021-11-28 01:14:36 -0800

    initial

--- app/src/main/java/com/example/spokes/MapsActivity.java
+++ app/src/main/java/com/example/spokes/MapsActivity.java
@@ -16,10 +16,13 @@ import android.graphics.Camera;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
+import com.google.android.gms.location.ActivityRecognition;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -38,6 +41,7 @@ import com.example.spokes.databinding.ActivityMapsBinding;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -48,18 +52,32 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     //Location Objects
     private FusedLocationProviderClient mFusedLocationClient;
     private LocationRequest mLocationRequest;
+    private boolean mInitial;
     private Marker mCurrentMarker;
-    private Location mLastLocation;
+    private Location mStart;
+    private Location mCurrent;
+    private Location mFinish;
 
     //Toolbar & Tracking Button
     private Toolbar mToolbar;
     private FloatingActionButton mTrack;
     private boolean mTracking;
 
+    private TextView mSpeedView;
+    private TextView mDistView;
+
     //Tracking Variables
     private double mSpeed;
     private double mDistance;
     private double mTime;
+
+    //Timer
+    private Handler mTimeHandler;
+    private TextView mTimeView;
+    private String mTimeString;
+
+    //To Storage --> Firebase
+    private List<Object> mMetrics;
 
     /** Lifecycle Functions */
 
@@ -74,28 +92,70 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mToolbar = findViewById(R.id.appToolbar);
         setSupportActionBar(mToolbar);
 
+        //Initialization
+        mStart = new Location("");
+        mFinish = new Location("");
+
+        //Timer
+        mTime = 0;
+        mTimeView = findViewById(R.id.timer);
+        mTimeView.setText(getTime(mTime));
+        timerSetup();
+
         //Tracking button set up
         mTrack = findViewById(R.id.track);
         mTrack.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
                 if (mTracking){
-                    //Save all info switch activities to summary screen
                     mTrack.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.teal_200)));
-                    //mTrack.setImageResource(R.drawable.play);
-                    mTracking = false;
+                    Log.d ("Tracked", "Distance: " + distance(mFinish, mStart));
+                    //Save all info switch activities to summary screen --> Firebase implementation / SharedPreferences
+                    mFinish = mCurrent;
+                    mTime = 0; mDistance = 0; mTracking = false;
+
+                    //Revert Map Settings
+                    mMap.getUiSettings().setAllGesturesEnabled(true);
+                    mMap.getUiSettings().setCompassEnabled(true);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(true);
+                    //Clear all text views
+                    clearViews();
+
+
                 }
                 else{
                     //Start timer, distance, and speed tracking
                     mTrack.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.red)));
-                    //mTrack.setImageResource(R.drawable.stop);
+                    //Change Camera Position
+                    LatLng latLng = new LatLng(mCurrent.getLatitude(), mCurrent.getLongitude());
+                    CameraPosition mCamera = new CameraPosition.Builder()
+                            .target(latLng)
+                            .zoom(18)
+                            .tilt(40)
+                            .build();
+                    mMap.animateCamera(CameraUpdateFactory.newCameraPosition(mCamera));
+                    mMap.getUiSettings().setAllGesturesEnabled(false);
+                    mMap.getUiSettings().setCompassEnabled(false);
+                    mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+                    mStart = mCurrent;
+                    //Update TextViews
+                    mSpeedView.setText(getSpeed(mSpeed));
+                    mDistView.setText(getDistance(mDistance));
                     mTracking = true;
+
                 }
             }
         });
 
+        //Performance Metrics Set up
+        mSpeedView = findViewById(R.id.speed);
+        mDistView = findViewById(R.id.distance);
+
         //Location Services
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        clearViews();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -144,12 +204,13 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mInitial = true;
 
         //Setup Location Request
         mLocationRequest = LocationRequest.create();
-        mLocationRequest.setInterval(100);
-        mLocationRequest.setFastestInterval(100);
-        mLocationRequest.setSmallestDisplacement(1);
+        mLocationRequest.setInterval(500);
+        mLocationRequest.setFastestInterval(250);
+        mLocationRequest.setSmallestDisplacement(2);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
         startLocationUpdates();
@@ -167,34 +228,70 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 //Last Location in the list is the most current location
                 Location location = locationList.get(locationList.size() - 1);
                 //Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
-                mLastLocation = location;
-                if (mCurrentMarker != null) {
-                    mCurrentMarker.remove();
-                }
+                mCurrent = location;
 
                 //Create a LatLng Object based on current Location
                 LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                //Make a Marker if Needed
-//                MarkerOptions markerOptions = new MarkerOptions();
-//                markerOptions.position(latLng);
-//                markerOptions.title("Current Position");
-//                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-//                mCurrentMarker = mMap.addMarker(markerOptions);
 
-                //Change Camera Position
-                CameraPosition mCamera = new CameraPosition.Builder()
-                        .target(latLng)
-                        .zoom(18)
-                        .tilt(40)
-                        .build();
-                mMap.moveCamera(CameraUpdateFactory.newCameraPosition(mCamera));
+                if (mInitial){
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17));
+                    mInitial = false;
+                }
 
-                Log.i("MapsActivity", "Speed: " + convertMiles(location.getSpeed()));
+                if (mTracking){
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    //Find Speed in m/s
+                    mDistance = distance(mCurrent, mStart);
+                    mSpeed = location.getSpeed();
+                    //Update TextViews
+                    mSpeedView.setText(getSpeed(mSpeed));
+                    mDistView.setText(getDistance(mDistance));
+                    Log.d ("Tracked", "Distance: " + mDistance);
+                }
             }
         }
     };
 
+    //Calculate Distance
+    public double distance(Location to, Location from){
+        double lng = Math.pow((from.getLongitude() - to.getLongitude()), 2);
+        double lat = Math.pow((from.getLatitude() - to.getLatitude()), 2);
+        return Math.pow(lng+lat, (0.5));
+    }
+    //Draw Route
+    public void drawRoute(List<Location> route){
+    }
+
     /** Time Tracker */
+    public void timerSetup(){
+        mTimeHandler = new Handler();
+        mTimeHandler.post(new Runnable (){
+            @Override
+            public void run() {
+                // Increment if tracking
+                if (mTracking) {
+                    // Set the text
+                    mTimeView.setText(getTime(mTime));
+                    mTime++;
+                }
+                mTimeHandler.postDelayed(this, 1000);
+            }
+        });
+    }
+
+    /** String Conversions */
+    public String getDistance(double distance){
+        return String.format(Locale.getDefault(), "%.2f m", distance);
+    }
+    public String getSpeed(double speed){
+        //Use activity recognition to get more accurate results (if time permits)
+        return String.format(Locale.getDefault(), "%.2f m/s", speed);
+    }
+    public String getTime(double time){
+        mTimeString = String.format(Locale.getDefault(), "%02d:%02d:%02d",
+                (int) time / 3600, (int) (time % 3600) / 60, (int) time % 60);
+        return mTimeString;
+    }
 
     /** Firebase TODO: Need to figure out how to work this so everything is stored within app */
 
@@ -228,5 +325,11 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
     private double convertMeters(double miles){
         return miles/1609.34;
+    }
+
+    private void clearViews(){
+        mTimeView.setText("");
+        mSpeedView.setText("");
+        mDistView.setText("");
     }
 }
